28e3534d66a19e47f13a3a6f3f16153ee99d105b
Aaron Ayeni <aaronayeni135@gmail.com>
2024-06-06 12:21:40 +0100

    add to line 6

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("add to line 4");
+
+        System.out.println("add to line 6 on feature b");
     }
 }
